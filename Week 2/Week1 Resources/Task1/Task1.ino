fbd58eac8c4fff6c2df44390f4d30985053d8021
MinMinis <tranthanhminh17072003@gmail.com>
2023-10-01 23:12:16 +0700

    Done part of Research Paper

--- Week 2/Week1 Resources/Task1/Task1.ino
+++ Week 2/Week1 Resources/Task1/Task1.ino
@@ -1,11 +1,12 @@
 // Task 1 writing program for blinking LED using pin number 2
-
+int led = 8;
+int time = 100;
 void setup() 
 {
    // put your setup code here, to run once:
    
    // Set pin number 2 as output to send current to LED
-   pinMode(2,OUTPUT);
+   pinMode(led,OUTPUT);
 }
 
 void loop() 
@@ -13,14 +14,14 @@ void loop()
     // put your main code here, to run repeatedly:
 
     // Start sending current to LED via pin number 2
-    digitalWrite(2,HIGH); 
+    digitalWrite(led,HIGH); 
 
     // Delay the program for 1 second
-    delay(1000); 
+    delay(time); 
 
     // Stop sending current to LED via pin number 2
-    digitalWrite(2,LOW); 
+    digitalWrite(led,LOW); 
 
     // Delay the program for 1 second
-    delay(1000); 
+    delay(time); 
 }
